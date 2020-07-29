ee26ef217df4417fc5f5b861219f36cac4807a6a
GaddamKishore <68538613+GaddamKishore@users.noreply.github.com>
2020-07-29 15:56:21 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+table_creation_sql = "create table Capitals(State text, Capital_city text)"
+c.execute(table_creation_sql)
+insert_row_sql = "insert into Capitals values('Telangana','Hyderabad')"
+c.execute(insert_row_sql)
+insert_row_sql = "insert into Capitals values('Tamilnadu','Chennai')"
+c.execute(insert_row_sql)
